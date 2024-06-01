4e9be24c429898d6bffaa817e6c7e43f8d4b96ab
bookkuun <bookkuun@gmail.com>
2024-06-01 20:27:47 +0900

    初期設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js",
+        },
+      ],
+    },
+  },
+});
